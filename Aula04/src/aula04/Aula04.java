f1e45dd9e55e93e54141133fea0b194f235e06a8
Manuel Duarte <manuelgarcia.duarte1@gmail.com>
2022-05-16 16:55:26 -0300

    Estudando Java POO

--- Aula04/src/aula04/Aula04.java
+++ Aula04/src/aula04/Aula04.java
@@ -6,10 +6,14 @@ public class Aula04 {
 
 
     public static void main(String[] args) {
-        Caneta c1 = new Caneta();
-        c1.setModelo("BIC");
-        c1.setPonta(0.5f);
+        Caneta c1 = new Caneta("NIC", "Amarelo", 0.4f);
+        //c1.setModelo("BIC");
+        //c1.modelo = ("BIC"); programação alternativa para tipo publico
+        //c1.setPonta(0.5f);
+        //c1.status();
         c1.status();
+        Caneta c2 = new Caneta ("KKK", "Laranja", 1.5f);
+        c2.status();
     }
     
 }
--- Aula04/src/aula04/Caneta.java
+++ Aula04/src/aula04/Caneta.java
@@ -4,6 +4,16 @@ package aula04;
 public class Caneta {
     public String modelo;
     private float ponta;
+    private String cor;
+    private boolean tampada;
+    
+    public Caneta(String m, String c, float p){ // Este é o método Construtor
+        this.cor = c;
+        this.modelo = m;
+        this.ponta = p;
+        this.tampar();
+        
+    }
     
     public String getModelo(){
         return this.modelo;
@@ -17,9 +27,17 @@ public class Caneta {
     public void setPonta(float p){
         this.ponta = p;
     }
+    public void tampar(){
+        this.tampada = true;
+    }
+    public void destampar() {
+        this.tampada = false;
+    }    
     public void status(){
         System.out.println("Sobre a Caneta:");
         System.out.println("Modelo:" + this.modelo);
         System.out.println("Ponta:" + this.ponta);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tamapada: " + this.tampada);
     }
 }
