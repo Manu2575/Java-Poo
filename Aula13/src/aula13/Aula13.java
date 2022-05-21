95c526b2a961ff5afc07a575b4cb0f2c1eba84b2
Manuel Duarte <manuelgarcia.duarte1@gmail.com>
2022-05-21 16:56:53 -0300

    Estudando Java POO

--- Aula13/src/aula13/Aula13.java
+++ Aula13/src/aula13/Aula13.java
@@ -11,6 +11,14 @@ public class Aula13 {
         m.emitirSom();
         l.emitirSom();
         c.emitirSom();
+        
+        c.reagir("Ola");
+        c.reagir("Vai apanhar");
+        c.reagir(11, 45);
+        c.reagir(21, 00);
+        c.reagir(false);
+        c.reagir(2, 12.5f);
+        c.reagir(17, 4.5f);
    
     }
     
